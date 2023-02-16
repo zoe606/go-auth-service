6052eeb5105e0accee3901f18c6b8f14fd9cfe3b
librantara <erlangga@docotel.com>
2023-02-16 13:38:46 +0700

    Init auth service

---
+++ cmd/api/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const webPort = "80"
+
+var counts int64
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting auth service")
+	conn := connectToDb()
+	if conn == nil {
+		log.Panic("Cant connect to pgsql")
+	}
+
+	//setup config
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDb() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDb(dsn)
+		if err != nil {
+			log.Println("Pgsql not ready...")
+			counts++
+		} else {
+			log.Println("Pgsql connected")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off two seconds...")
+		time.Sleep(2 * time.Second)
+		continue
+	}
+}
